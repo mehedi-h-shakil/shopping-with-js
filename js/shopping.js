e69223fb59b57971b6ece5f54a0d8d7798159d16
sneoo <epicshakil@gmail.com>
2022-08-21 18:19:42 +0600

    done

--- js/shopping.js
+++ js/shopping.js
@@ -5,20 +5,19 @@ function display(cartProduct) {
     const tableBody = document.getElementById('cart-products');
     tableBody.innerHTML = "";
     for (let i = 0; i < cartProduct.length; i++) {
-        const name = cartArray[i].productName;
-        const price = cartArray[i].productPrice;
+        const name = cartProduct[i].productName;
+        const price = cartProduct[i].productPrice;
 
         totalPrice = totalPrice + price;
-
         const tr = document.createElement("tr");
+
         tr.innerHTML = `
         <th>${i + 1}</th>
         <td>${name}</td>
         <td>${price}</td>
         `;
-        tableBody.appendChild(tr)
+        tableBody.appendChild(tr);
     }
-
     const tr = document.createElement("tr");
 
     tr.innerHTML = `
@@ -26,6 +25,7 @@ function display(cartProduct) {
     <td>Total Price</td>
     <td>${totalPrice}</td>
     `;
+
     tableBody.appendChild(tr);
 }
 
@@ -37,10 +37,10 @@ function addToCart(element) {
         productName: productName,
         productPrice: parseFloat(productPrice)
     }
-
     cartArray.push(productObj);
 
     document.getElementById('total-added-product').innerText = cartArray.length;
 
     display(cartArray);
+
 }
